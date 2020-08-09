891cfdb9afab4ad88ee460efef184a05101b84f8
Conner Isaacs <cjisaacs@att.net>
2020-08-09 00:15:42 -0400

    added rotating content

--- javascript/UI.js
+++ javascript/UI.js
@@ -9,12 +9,16 @@ function prevClicked(){
 	let next = document.getElementsByClassName("navigation__next")[0];
 	let prev = document.getElementsByClassName("navigation__prev")[0];
 	let active = document.getElementsByClassName("navigation__active")[0];
-
+    let prevMain = document.getElementsByClassName("prevMain")[0];
+	let currentMain = document.getElementsByClassName("currentMain")[0];
+	let nextMain = document.getElementsByClassName("nextMain")[0];
 	//Remove all active animations
 	prev.style.animation = null;
 	active.style.animation = null;
 	next.style.animation = null;
-
+    prevMain.style.animation = null;
+	currentMain.style.animation = null;
+	nextMain.style.animation = null;
 	//Remove active Event Listeners
 	next.removeEventListener("click", nextClicked);
 	prev.removeEventListener("click", prevClicked);
@@ -27,7 +31,15 @@ function prevClicked(){
 	
 	active.classList.toggle("navigation__active");
     active.classList.toggle("navigation__next");
+    
+    nextMain.classList.toggle("nextMain");
+	nextMain.classList.toggle("prevMain");
 	
+	prevMain.classList.toggle("prevMain");
+	prevMain.classList.toggle("currentMain");
+	
+	currentMain.classList.toggle("currentMain");
+	currentMain.classList.toggle("nextMain");
 	//Adjust Event Listeners
 	next.addEventListener("click", prevClicked);
 	active.addEventListener("click", nextClicked);
@@ -36,17 +48,27 @@ function prevClicked(){
 	active.style.animation = "nav_activeToNext .66s ease-in-out";
 	next.style.animation = "nav_nextToPrev .66s ease-in-out";
 	
+	prevMain.style.animation = "main_prevToActive .66s ease-in-out";
+	currentMain.style.animation = "main_activeToNext .66s ease-in-out";
+	nextMain.style.animation = "main_nextToPrev .66s ease-in-out";
+	
+	prevMain.style.transition = "background .66s .33s";
 }
 
 function nextClicked(){
 	let next = document.getElementsByClassName("navigation__next")[0];
 	let prev = document.getElementsByClassName("navigation__prev")[0];
 	let active = document.getElementsByClassName("navigation__active")[0];
-
+    let prevMain = document.getElementsByClassName("prevMain")[0];
+	let currentMain = document.getElementsByClassName("currentMain")[0];
+	let nextMain = document.getElementsByClassName("nextMain")[0];
+	
 	prev.style.animation = null;
 	active.style.animation = null;
 	next.style.animation = null;
-
+    prevMain.style.animation = null;
+	currentMain.style.animation = null;
+	nextMain.style.animation = null;
 	//remove all event listeners
 	prev.removeEventListener("click", prevClicked);
 	next.removeEventListener("click", nextClicked);
@@ -58,13 +80,27 @@ function nextClicked(){
 	prev.classList.toggle("navigation__next");
 	
 	active.classList.toggle("navigation__active");
-  active.classList.toggle("navigation__prev");
+    active.classList.toggle("navigation__prev");
 
+    nextMain.classList.toggle("nextMain");
+    nextMain.classList.toggle("currentMain");
+    
+    prevMain.classList.toggle("prevMain");
+    prevMain.classList.toggle("nextMain");
+    
+    currentMain.classList.toggle("currentMain");
+    currentMain.classList.toggle("prevMain");
+  
 	//Adjust Event Listeners
 	prev.addEventListener("click", nextClicked);
 	active.addEventListener("click", prevClicked);
 	
 	prev.style.animation = "nav_prevToNext .66s ease-in-out";
 	active.style.animation = "nav_activeToPrev .66s ease-in-out";
-	next.style.animation = "nav_nextToActive .66s ease-in-out";
+    next.style.animation = "nav_nextToActive .66s ease-in-out";
+    
+    prevMain.style.animation = "main_prevToNext .66s ease-in-out";
+	currentMain.style.animation = "main_activeToPrev .66s ease-in-out";
+	nextMain.style.animation = "main_nextToActive .66s ease-in-out";
+	nextMain.style.transition = "background .66s .33s";
 }
